ed093b7a9e324f7ecb4cd496d021bd2ed6caa5bb
khoinguyenAdonis <nguyendinhkhoi1302@gmail.com>
2023-12-03 13:11:04 +0700

    khoi

--- exercise_Cpp/QuanlySV2.0/main.cpp
+++ exercise_Cpp/QuanlySV2.0/main.cpp
@@ -52,12 +52,13 @@ void Student::classification(){
     else classify = "YEU";
 }
 
-void Student::displayIfo(){
-    cout<<"Ho & Ten: "<< name;
-    cout<<" | Tuoi    : "<< age;
+void Student::displayIfo(){    
+    cout<<" | ID : "<<id;
+    cout<<" | Ho & Ten : "<< name;
+    cout<<" | Tuoi : "<< age;
     cout<<" | Diem TB : "<< mediumScore;
-    cout<<" | Hoc Luc : "<< classify;
-    cout<<" | ID cua sinh vien la: "<<id<<endl;
+    cout<<" | Hoc Luc : "<< classify<<endl;
+
 }
 int Student:: Id(){
         return id;
@@ -78,6 +79,7 @@ class StudentManagement {
         void removeStudent();
         void editStudent();
         void arrangeName();
+        void arrangeID();
         void display();
 };
 
@@ -191,6 +193,21 @@ void StudentManagement::arrangeName(){
     }
 }
 
+void StudentManagement::arrangeID(){   
+    vector<Student> temp;
+    for (int i = 0; i < array.size(); i++){
+        int j = i+1;
+        for (j; j< array.size(); j++){
+            if(array[i].Id() > array[j].Id()){
+                temp.push_back(array[i]);
+                array[i] = array[j];
+                array[j] = temp[0];
+            }
+            temp.clear();
+        }
+    }
+}
+
 void graphic(){
      int option; 
     StudentManagement a;
@@ -201,10 +218,10 @@ void graphic(){
         cout<<"| *CHON CHUC NANG CHUONG TRINH:                   |"<<endl;
         cout<<"|>1. THEM SINH VIEN                               |"<<endl;
         cout<<"|>2. XOA SINH VIEN                                |"<<endl;
-        cout<<"|>3. SUA THONG TN SIN VIEN                        |"<<endl;
-        cout<<"|>4. SAP XEP SINH VIEN THEO TEN                   |"<<endl;
+        cout<<"|>3. SUA THONG TIN SINH VIEN                      |"<<endl;
+        cout<<"|>4. SAP XEP SINH VIEN                            |"<<endl;
         cout<<"|>5. HIEN THI DANH SACH SINH VIEN                 |"<<endl;
-        cout<<"|>6. THOAT CHUOWNG TRINH                          |"<<endl;
+        cout<<"|>6. THOAT CHUONG TRINH                           |"<<endl;
         cout<<"---------------------------------------------------"<<endl;
         cout<<"   NHAP CHUC NANG CHUONG TRINH: ";
         cin>>option;
@@ -226,7 +243,7 @@ void graphic(){
                 cout<<"|                                                 |"<<endl;
                 cout<<"|                                                 |"<<endl;
                 cout<<"---------------------------------------------------"<<endl;
-                cout<<"   NHAP CHUC NANG CHUONT TRNH: ";
+                cout<<"   NHAP CHUC NANG CHUONG TRINH: ";
                 cin>>option;
                 cin.ignore();
                 cout<<"---------------------------------------------------"<<endl; 
@@ -271,14 +288,60 @@ void graphic(){
             break;
         case 4:
             system("clear");
-            cout<<"----------------SAP_XEP_SINH_VIEN_THEO_TEN----------------"<<endl;
-            a.arrangeName();
+            cout<<"----------------SAP_XEP_SINH_VIEN----------------"<<endl;
+            do
+            {
+                system("clear");
+                option = 0;
+                cout<<"----------------SAP_XEP_SINH_VIEN------------------"<<endl;
+                cout<<"| *CHON CHUC NANG CHUONG TRINH:                   |"<<endl;
+                cout<<"|>1. SAP XEP THEO TEN                             |"<<endl;
+                cout<<"|>2. SAP XEP THEO ID                              |"<<endl;
+                cout<<"|>3. EXIT                                         |"<<endl;
+                cout<<"|                                                 |"<<endl;
+                cout<<"|                                                 |"<<endl;
+                cout<<"|                                                 |"<<endl;
+                cout<<"---------------------------------------------------"<<endl;
+                cout<<"   NHAP CHUC NANG CHUONG TRINH: ";
+                cin>>option;
+                cin.ignore();
+                cout<<"---------------------------------------------------"<<endl; 
+                if(option == 1){
+                    system("clear");
+                    cout<<"----------------SAP XEP THEO TEN----------------"<<endl;
+                    a.arrangeName();
+                    cout<<"          --------------------------"<<endl; 
+                    cout<<"          | DA SAP XEP THANH CONG! |"<<endl;
+                    cout<<"          --------------------------"<<endl;
+                    cout<<"            NHAP PHIM *3* DE THOAT :";
+                    cin>>option;
+                    cin.ignore();
+                }
+                else if (option == 2 ){
+                    system("clear");
+                    cout<<"----------------SAP XEP THEO ID----------------"<<endl;
+                    a.arrangeID();
+                    cout<<"          --------------------------"<<endl; 
+                    cout<<"          | DA SAP XEP THANH CONG! |"<<endl;
+                    cout<<"          --------------------------"<<endl;
+                    cout<<"            NHAP PHIM *3* DE THOAT :";
+                    cin>>option;
+                    cin.ignore();
+                }
+                else if (option == 3){
+                    system("clear");
+                    break;
+                }
+            /* code */
+        } while (option >0 && option <4);
+                        
             break;
         case 5:
             system("clear");
-            cout<<"----------------HIEN_THI_DANH_SACH_SINH_VIEN----------------"<<endl;
+            cout<<"----------------DANH_SACH_SINH_VIEN---------------------------------------------"<<endl;
             a.display();
-            cout<<"----------------NHAP_PHIM_BAT_KY_DE_THOAT------------------------------"<<endl;
+            cout<<"--------------------------------------------------------------------------------"<<endl; 
+            cout<<"NHAP PHIM *3* DE THOAT :";
             cin>>option;
             break;
         case 6:
